62071bd4ac55be007fce226e431fd4a8be2794c6
taufiqlibrary <taufiq.library@gmail.com>
2021-08-10 17:22:41 +0700

    Learn Python
    Learn Python

---
+++ range_function_sample2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 17:04:40 2021
+
+@author: taufiqlibrary
+"""
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
