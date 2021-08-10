344dff32adc2d60c05aa9cb924726e4cce4f3729
taufiqlibrary <taufiq.library@gmail.com>
2021-08-10 17:22:17 +0700

    Learn Python
    Learn Python

---
+++ Looping_sample1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 17:11:59 2021
+
+@author: taufiqlibrary
+"""
+
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+for k, v in knights.items():
+    print(k, v)
