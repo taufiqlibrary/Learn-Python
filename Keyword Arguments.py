f44e60819f1a6af214ea04d13b14ec742e5015b5
taufiqlibrary <taufiq.library@gmail.com>
2021-08-10 17:22:08 +0700

    Learn Python
    Learn Python

---
+++ Keyword Arguments.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 16:49:34 2021
+
+@author: taufiqlibrary
+"""
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+        
+cheeseshop("Limburger", "It's very runny, sir.",
+         "It's really very, VERY runny, sir.",
+         shopkeeper="Michael Palin",
+         client="John Cleese",
+         sketch="Cheese Shop Sketch")
